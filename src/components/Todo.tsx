376251d2bdfbf882df36d738c06c64647c3bbf69
kavvata <gabriel.kawata@gmail.com>
2025-10-09 20:50:12 -0300

    feat: criado componente de TODO

---
+++ src/components/Todo.tsx
@@ -0,0 +1,7 @@
+export const Todo: React.FC = () => {
+  return (
+    <div className="max-w-7xl flex flex-col items-center">
+      <span className="font-kare text-3xl ">TODO</span>
+    </div>
+  );
+};
