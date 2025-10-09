b88b5db534843aa4f3f2eddc0afb6d8c5cd1e481
kavvata <gabriel.kawata@gmail.com>
2025-10-09 20:30:03 -0300

    feat(home): iterated over a bunch of writing ideas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import GameOfLife from "../components/GameOfLife";
-import { KarenBold } from "../components/KarenBold";
-import imgURL from "../../public/cappella_sistina_halftone.png";
+import { KareBold } from "../components/KareBold";
 
 function Home() {
-  const BASE_URL = import.meta.env.BASE_URL;
   return (
     <main className="flex-grow space-y-14 px-4 py-24 leading-relaxed">
-      <div className="flex flex-col lg:flex-row lg:gap-8 lg:mx-16">
+      <div className="flex flex-col justify-between lg:flex-row lg:gap-8 lg:mx-16">
         <section className="flex flex-col mb-16 lg:mb-0 max-w-72">
           <header className="">
             <h1 className="font-kare text-5xl font-bold text-nowrap">
@@ -15,37 +13,58 @@ function Home() {
             <h2 className="text-xl font-medium text-nowrap">Fullstack Dev</h2>
 
             <p className="mt-4 text-zinc-300">
-              Desenvolvedor Full-Stack movido a{" "}
-              <KarenBold>curiosidade</KarenBold>.{" "}
+              Desenvolvedor Full-Stack movido a <KareBold>curiosidade</KareBold>
+              .{" "}
             </p>
           </header>
+          <div className="mt-11">
+            <ul className="flex flex-row gap-2 text-sm text-zinc-400">
+              <li>
+                <a
+                  href="https://github.com/kavvata/"
+                  target="_blank"
+                  className="hover:text-zinc-100 transition-colors"
+                >
+                  <span className="font-kare text-lg">[g]</span> Github
+                </a>
+              </li>
+              <div className="border-0 border-r text-zinc-500" />
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/gabriel-kavata-leandro-734256286"
+                  className="hover:text-zinc-100 transition-colors"
+                  target="_blank"
+                >
+                  <span className="font-kare text-lg">[l]</span> LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
         </section>
 
         <section className="flex flex-col gap-4 flex-grow max-w-7xl">
           <GameOfLife />
           <div className="text-zinc-400">
-            <p className="text-zinc-200">Apaixonado por arte.</p>
-            <p>
-              Acredito que, onde há o toque do ser humano, inconsequentemente
-              existe <KarenBold>expressão</KarenBold>. Observo isso nas mais
-              antigas obras de arte. Os maiores museus do mundo estão rechados
-              com produtos de marceneiros, pintores, vidreiros -{" "}
-              <KarenBold>Trabalho</KarenBold>.
+            <p className="text-zinc-200">
+              Entusiasta de arquiteturas escaláveis.
             </p>
-            <div className="mt-8 space-y-2">
-              <figure>
-                <img
-                  className="w-full"
-                  src={imgURL}
-                  alt="Imagem do teto da capela sistina."
-                />
-              </figure>
-              <p className="italic text-center text-sm text-zinc-500">
-                Exemplo de Trabalho.
-              </p>
-            </div>
             <p>
-              O conceito do artista surgiu somente após a revolução industrial.
+              Busco projetar e implementar sistemas robustos, combinando boas
+              práticas de <KareBold>backend</KareBold>, automação e{" "}
+              <KareBold>observabilidade</KareBold>. Trabalho com ambientes{" "}
+              <KareBold>containerizados</KareBold> e fluxos de DevOps para criar
+              infraestruturas previsíveis, fáceis de manter e preparadas para
+              crescer com o produto.
+            </p>
+            <p className="text-zinc-200 mt-8">
+              Dedicado a interfaces que fazem sentido.
+            </p>
+            <p>
+              Busco criar experiências coerentes, onde cada elemento da
+              interface reflete uma intenção clara. Tenho interesse em{" "}
+              <KareBold>componentização</KareBold> e arquitetura de front-end
+              priorizando <KareBold>empatia</KareBold>{" "}
+              <KareBold>técnica</KareBold> e consistência na interação.
             </p>
           </div>
         </section>
