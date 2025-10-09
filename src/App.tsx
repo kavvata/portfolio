62189aa551480c242af9a693fb75ed6b7b4f1239
kavvata <gabriel.kawata@gmail.com>
2025-10-09 20:28:51 -0300

    feat: added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Experience from "./pages/Experience";
 import Navbar from "./components/Navbar";
+import About from "./pages/About.tsx";
+import Projects from "./pages/Projects.tsx";
+import Blog from "./pages/Blog.tsx";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/experience/" element={<Experience />} />
+          <Route path="/projects/" element={<Projects />} />
+          <Route path="/blog/" element={<Blog />} />
+          <Route path="/about/" element={<About />} />
         </Routes>
         <Footer />
       </BrowserRouter>
