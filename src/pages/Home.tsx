3ae40a0377e9e6dc9453bd0408cc73a15f9cf9be
kavvata <gabriel.kawata@gmail.com>
2025-10-10 01:28:17 -0300

    fix(home): removed ai slop and wrote my own text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import GameOfLife from "../components/GameOfLife";
 import { KareBold } from "../components/KareBold";
+import imgCappellaSistinaUrl from "../../public/cappella_sistina_halftone.png";
 
 function Home() {
   return (
     <main className="flex-grow space-y-14 px-4 py-24 leading-relaxed">
-      <div className="flex flex-col justify-between lg:flex-row lg:gap-8 lg:mx-16">
+      <div className="flex flex-col lg:flex-row lg:gap-8 lg:mx-16">
         <section className="flex flex-col mb-16 lg:mb-0 max-w-72">
           <header className="">
             <h1 className="font-kare text-5xl font-bold text-nowrap">
@@ -47,26 +48,68 @@ function Home() {
             <GameOfLife />
           </div>
           <div className="text-zinc-400">
+            <p className="text-zinc-200">Apaixonado por arte.</p>
+            <p>
+              Acredito que, onde há o toque do ser humano, inconsequentemente
+              existe <KareBold>expressão</KareBold>. Observo isso nas mais
+              antigas obras de arte. Os maiores museus do mundo estão rechados
+              com produtos de marceneiros, pintores, vidreiros -{" "}
+              <KareBold>Trabalho</KareBold>.
+            </p>
+          </div>
+          <div className="mt-8 space-y-2">
+            <figure>
+              <img
+                className="w-full rounded-lg"
+                src={imgCappellaSistinaUrl}
+                alt="Imagem do teto da capela sistina em bitmap, com o tema de cores Kanagawa."
+              />
+            </figure>
+            <p className="text-center mb-8 text-sm text-zinc-500 italic">
+              Exemplo de "Trabalho".
+            </p>
+          </div>
+          <div className="text-zinc-400">
             <p className="text-zinc-200">
-              Entusiasta de arquiteturas escaláveis.
+              Sinto que o mesmo existe dentro de sistemas.
             </p>
             <p>
-              Tenho interesse em sistemas que se sustentam no tempo, construídos
-              com boas práticas de <KareBold>backend</KareBold>, automação e{" "}
-              <KareBold>observabilidade</KareBold>. Trabalho em ambientes{" "}
-              <KareBold>containerizados</KareBold>, buscando previsibilidade,
-              manutenção simples e crescimento orgânico junto ao produto.
-            </p>
-            <p className="text-zinc-200 mt-8">
-              Dedicado a interfaces que fazem sentido.
+              Por trás de cada rotina, estrutura de dados, wireframe e layout
+              existe uma <KareBold>decisão</KareBold>, repleto de intenções
+              humanas. A cada decisão, humores, personalidades e características
+              do ambiente são expressas - as vezes se revelam em commits e
+              blocos de comentários - muitas vezes evidente no próprio
+              código-fonte.
             </p>
             <p>
-              Busco enxergar o front-end como um espaço de equilíbrio entre
-              intenção e clareza. Gosto de pensar a{" "}
-              <KareBold>atomicidade</KareBold> e a arquitetura de interface como
-              meios de traduzir propósito, com{" "}
-              <KareBold>empatia técnica</KareBold> e consistência na
-              experiência.
+              Porém, não só de código-fonte é composto o{" "}
+              <KareBold>trabalho</KareBold> do dev. Existe um universo de
+              contexto que não se traduz para o código, nem documentação ou
+              prompt files. Naur argumentava que esses artefatos são{" "}
+              <KareBold>estáticos</KareBold>, sendo uma grande contradição ao
+              fato de que o mundo é um <KareBold>verbo</KareBold>. Problemas
+              mutam constantemente.
+            </p>
+          </div>
+          <div className="text-zinc-400">
+            <p className="text-zinc-200">
+              Assim, busco construir sistemas que se sustentam no tempo.
+            </p>
+            <p>
+              Projetando arquiteturas robustas de <KareBold>back-end</KareBold>,
+              mantendo automações, observabilidade e ambientes containerizados
+              para maior escalabilidade; Na outra ponta, aplicando conceitos de
+              design atômico e empatia técnica ao <KareBold>front-end</KareBold>
+              , visando entregar uma experiência consistente que conversa com
+              usuário final, e sempre mantendo <KareBold>comunicação</KareBold>{" "}
+              <KareBold>ativa</KareBold> com colegas, gestores e usuários, pois
+              acredito que é a partir das conversas interdisciplinares que o
+              software se edifica.
+            </p>
+            <p>
+              No fim, sigo curioso pelas formas que o pensamento pode tomar em
+              um sistema, <KareBold>trabalhando</KareBold> em meus projetos e
+              compartilhando o que aprendo pelo caminho.
             </p>
           </div>
         </section>
